8e9e3f80b401697334fd5d5f3cdc15089c7e87eb
adferatuz <viadfeur@gmail.com>
2025-12-19 12:53:58 -0500

    fix: Inyectando nueva url para solucionar problemas de cors

--- tecnoreservas-server/src/main/java/com/farukgenc/boilerplate/springboot/configuration/SecurityConfiguration.java
+++ tecnoreservas-server/src/main/java/com/farukgenc/boilerplate/springboot/configuration/SecurityConfiguration.java
@@ -106,7 +106,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://frontend:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
